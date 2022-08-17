46811c1b4f87c4e65b1461086e641b40651ebddc
Alikan Tunali <Alikan.Tunali@codit.eu>
2022-08-18 00:57:52 +0200

    webapp deployment added to pipe.

--- src/CarsAPI/Program.cs
+++ src/CarsAPI/Program.cs
@@ -9,8 +9,9 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 //services collection will be configured below. Service is a component. 
